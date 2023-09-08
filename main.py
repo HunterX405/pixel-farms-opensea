ff3d2687c269e44a352c11edac4f67fde9b72ffd
HunterX405 <hunterdev405@gmail.com>
2023-09-09 00:09:05 +0800

    pixels-farm-opensea

---
+++ main.py
@@ -0,0 +1,89 @@
+from nicegui import ui
+from requests import Session
+import time
+
+# Network Session
+s = Session()
+
+# Pagination
+start = 0
+limit = 10
+
+
+def get_api(url, p=None, s=Session()):
+    conn = s.get(url, params=p)
+    while not conn.ok:
+        print(conn.reason)
+        time.sleep(5)
+        conn = s.get(url, params=p)
+    return conn.json()
+
+
+def get_opensea_listings(collection, s=Session()):
+    listings_url = f"https://api.opensea.io/v2/listings/collection/{collection}/all"
+    s.headers.update({
+        "accept": "application/json",
+        "X-API-KEY": "d6f7308aebe3444ea7fee808388e3138"
+    })
+    opensea_api = get_api(listings_url, s=s)
+    listings = opensea_api['listings']
+    while "next" in opensea_api:
+        print('next')
+        opensea_api = get_api(
+            listings_url, {'next': opensea_api['next']}, s)
+        listings += opensea_api['listings']
+
+    return sorted(listings, key=lambda l: float(l['price']['current']['value'])/1e18)
+
+
+def sync():
+    global listings
+    listings = get_opensea_listings('pixels-farm', s)  # collection slug
+    get_farms.refresh()
+
+
+def next():
+    global start
+    start += 10
+    if start > len(listings) - limit:
+        start = len(listings) - limit
+    get_farms.refresh()
+
+
+def prev():
+    global start
+    start -= 10
+    if start < 0:
+        start = 0
+    get_farms.refresh()
+
+
+@ui.refreshable
+def get_farms() -> None:
+    with ui.row():
+        ui.button('Previous Page',
+                  on_click=prev).enabled = True if start > 0 else False
+        ui.button('Next Page', on_click=next).enabled = False if start >= len(
+            listings) - limit else True
+        ui.button('Sync', on_click=sync)
+
+    with ui.row().style('justify-content: space-evenly'):
+
+        for i in range(start, start + limit):
+            id = listings[i]['protocol_data']['parameters']['offer'][0]['identifierOrCriteria']
+            price = float(listings[i]['price']['current']['value'])/1e18
+            with ui.card().tight().style('width: 18.5vw').classes('no-shadow border-[0px]'):
+                ui.html(
+                    f"<iframe width=100% height='270vh' src='https://play.pixels.xyz/pixels/share/{id}'></iframe>")
+                with ui.card_section():
+                    ui.label(f'Farm Land #{id}')
+                    ui.label(f'Price: {price:.4f} ETH')
+
+
+def main():
+    sync()
+    get_farms()
+    ui.run(title='Pixel Farms Opensea', dark=True,reload=False)
+
+
+main()
